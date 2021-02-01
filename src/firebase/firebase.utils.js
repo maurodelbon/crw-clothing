d55a72f401c9dcf90a9c2af7904710aef4f57a65
Mauro Delbon <maurodelbon@gmail.com>
2021-02-01 17:09:22 -0300

    I configured firestore

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,25 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+
+const config = {
+    apiKey: "AIzaSyDWiyUjXfNxatcmG2ErYm_7BMkOS4cdbw4",
+    authDomain: "commerce-35053.firebaseapp.com",
+    projectId: "commerce-35053",
+    storageBucket: "commerce-35053.appspot.com",
+    messagingSenderId: "154319464226",
+    appId: "1:154319464226:web:44da0c094295d9369ea77b",
+    measurementId: "G-067T233MZQ"
+  };
+
+  firebase.initializeApp(config);
+
+  export const auth = firebase.auth();
+  export const firestore = firebase.firestore();
+
+  const provider = new firebase.auth.GoogleAuthProvider();
+  provider.setCustomParameters({ prompt: 'select_account'});
+  export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+  export default firebase;
